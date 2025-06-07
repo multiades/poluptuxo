c691f64378b57c09ac92489d14f01264c62e42d7
multiades <92202404+multiades@users.noreply.github.com>
2025-06-07 14:22:31 +0300

    Add files via upload

---
+++ assets/scripts/navbar.js
@@ -0,0 +1,57 @@
+// Wait until all HTML elements are fully parsed, is not necessary if the script is at the end of the html's body but it does not add overhead
+// and it future-proofs the script.
+document.addEventListener('DOMContentLoaded', () => { 
+  const toggler = document.getElementById('navbar_toggle'); // The navbar's toggle hidden checkbox
+  const navbar = document.getElementById('navbar'); 
+
+  // Function that renders the navbar non-interactive when it is not expanded
+  function navInteractivity () { 
+    const isExpanded = toggler.checked;
+
+    // Block pointer interaction when collapsed
+    navbar.classList.toggle('inert', !isExpanded);
+
+    // Set or remove tab focus
+    navbar.querySelectorAll('a, label.dropdown').forEach(focusable => {
+      focusable.tabIndex = isExpanded ? 0 : -1;
+    });
+
+    // Screen reader visibility depending on the state of the navbar
+    navbar.setAttribute('aria-hidden', String(!isExpanded));
+
+    // Announcement of button state on screenreaders
+    toggler.setAttribute('aria-expanded', String(isExpanded));
+
+    // Uncheck all of the navbar's nested checkboxes on detraction
+    if (!isExpanded) {
+      navbar.querySelectorAll('input[type="checkbox"]').forEach(checkbox => {
+        checkbox.checked = false;
+      }); 
+    }
+  }
+
+  // Initial run
+  navInteractivity();
+
+  // Re-run on toggle
+  toggler.addEventListener('change', navInteractivity);
+});
+
+/*
+
+  Select all anchors and dropdown labels in the nav element
+  const nodes = document.querySelectorAll('nav a', 'nav label.dropdown');
+
+  function updateState() {
+    const expanded = toggle.checked;
+    links.forEach
+  
+  if (!navbar) return; // Exits the script if navbar does not exist
+sidebar.addEventListener('mouseleave', () => {
+  const checkboxes = navbar.querySelectorAll('input[type="checkbox"]');
+  
+  checkboxes.forEach(checkbox => {
+    checkbox.checked = false;
+  });
+});
+*/
